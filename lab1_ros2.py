2e63e4a41376fc4b4ae7f293521d03b779e39e65
IstanSlamet <istanslamet@gmail.com>
2026-02-02 15:33:55 -0500

    fixed braces

--- lab1_ros2.py
+++ lab1_ros2.py
@@ -12,9 +12,9 @@ node.stow_the_robot()
 node.move_to_pose({'joint_arm': 0.5, 'joint_lift': 1.1}, blocking=True)
 
 # Move all three wrist motors one at a time
-node.move_to_pose({'joint_wrist_yaw': np.radians(45), blocking=True})
-node.move_to_pose({'joint_wrist_pitch': np.radians(45), blocking=True})
-node.move_to_pose({'joint_wrist_roll': np.radians(45), blocking=True})
+node.move_to_pose({'joint_wrist_yaw': np.radians(45)}, blocking=True)
+node.move_to_pose({'joint_wrist_pitch': np.radians(45)}, blocking=True)
+node.move_to_pose({'joint_wrist_roll': np.radians(45)}, blocking=True)
 
 # Open gripper and close it
 node.move_to_pose({'joint_gripper_finger_left': np.radians(50)}, blocking=True)
@@ -32,14 +32,11 @@ node.stow_the_robot()
 
 # Drive forward 0.5m, rotate 180deg, drive 0.5m forward
  # Move robot base 0.2 meters forward
-node.move_to_pose({'translate_mobile_base': 0.2}, blocking=True)
+node.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
 
  # Rotate base by 30 degrees
 node.move_to_pose({'rotate_mobile_base': np.radians(180)}, blocking=True)
 
  # Move robot base 0.2 meters forward
-node.move_to_pose({'translate_mobile_base': 0.2}, blocking=True)
-<<<<<<< HEAD
+node.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
 
-=======
->>>>>>> d20ccf99ad95228023a3afcc2ac127e7bdf3801f
